db07482c654d59aded8f1f863544c4d7ef073bef
Mayowa-Temidayo <hallotaiwo@gmail.com>
2024-02-23 01:39:32 +0100

    data_types_assignment

---
+++ Assignments/week2assignment.dart
@@ -0,0 +1,31 @@
+void main(){
+ //integer data type and example
+  int x =3;
+  var y = 5;
+  print(x+y);
+
+  //double data type and example
+  double a = 5.2;
+  var b = 4.6;
+  print(a-b);
+
+  //string data type and example
+  String name = "Taiwo";
+  print('your name is ${name}');
+
+  //list data type and example
+  List<String> names = ["ojo", "ade", "taiwo"];
+  print(names);
+
+  //map data type and exanple
+  Map<String, int> ages = {
+  'Alice': 30,
+  'Bob': 25,
+  'Charlie': 35,
+  };
+
+  print(ages);
+
+
+
+}
